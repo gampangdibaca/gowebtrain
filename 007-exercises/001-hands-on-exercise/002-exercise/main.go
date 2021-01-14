86b2c7749c42986df0bcd4aaa26f2a392d3788c5
gampangdibaca <bimopangestukurniawan@gmail.com>
2021-01-14 21:35:15 +0700

    adding error handling on exercise 2 and 3

--- 007-exercises/001-hands-on-exercise/002-exercise/main.go
+++ 007-exercises/001-hands-on-exercise/002-exercise/main.go
@@ -2,21 +2,31 @@ package main
 
 import (
 	"html/template"
+	"log"
 	"net/http"
 )
 
 var tpl *template.Template
 
 func basic(w http.ResponseWriter, req *http.Request) {
-	tpl.ExecuteTemplate(w, "basic.gohtml", "this is basic path")
+	err := tpl.ExecuteTemplate(w, "basic.gohtml", "this is basic path")
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
 
 func dog(w http.ResponseWriter, req *http.Request) {
-	tpl.ExecuteTemplate(w, "dog.gohtml", "this is dog doggy dog path")
+	err := tpl.ExecuteTemplate(w, "dog.gohtml", "this is dog doggy dog path")
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
 
 func me(w http.ResponseWriter, req *http.Request) {
-	tpl.ExecuteTemplate(w, "me.gohtml", "GAMPANGDIBACA")
+	err := tpl.ExecuteTemplate(w, "me.gohtml", "GAMPANGDIBACA")
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
 
 func init() {
