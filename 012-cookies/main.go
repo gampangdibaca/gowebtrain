f69a79c7623a639f8ce683c3a2b192c3f8e3320e
gampangdibaca <bimopangestukurniawan@gmail.com>
2021-01-19 18:42:46 +0700

    finish learn cookies and doing the exercises

---
+++ 012-cookies/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", set)
+	http.HandleFunc("/read", read)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func set(w http.ResponseWriter, req *http.Request) {
+
+	http.SetCookie(w, &http.Cookie{
+		Name:  "my-cookie",
+		Value: "value of my cookie",
+	})
+	fmt.Fprintln(w, "COOKIE WRITTEN - CHECK YOUR BROWSER")
+	fmt.Fprintln(w, "open dev tools/ application/ cookies")
+}
+
+func read(w http.ResponseWriter, req *http.Request) {
+	c, err := req.Cookie("my-cookie")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+	}
+
+	fmt.Fprintln(w, "Your Cookie:", c)
+}
---
+++ 013-hands-on-exercise-cookies/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func foo(w http.ResponseWriter, req *http.Request) {
+	c, err := req.Cookie("my-cookie")
+	if err == http.ErrNoCookie {
+		c = &http.Cookie{
+			Name: "my-cookie",
+			Value: "0",
+		}
+	}
+	count, err := strconv.Atoi(c.Value)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	count++
+	c.Value = strconv.Itoa(count)
+
+	http.SetCookie(w, c)
+
+	fmt.Fprintln(w, "Your number of visit:", c.Value)
+	fmt.Fprintln(w, "COOKIE WRITTEN - CHECK YOUR BROWSER")
+	fmt.Fprintln(w, "open dev tools/ application/ cookies")
+
+}
